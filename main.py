f0c75ec87207c154a54ee575f5540190783736c3
lukasznawrocki <lukasz.nawrocki.pl@gmail.com>
2023-04-03 22:48:00 +0200

    Create columns, image and description

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1,col2=st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Łukasz Nawrocki")
+    content=""" Jakis kontent o mnie . aksd akdm ald lkams lkamskld maldm l dals
+    adlkasndkasndakndkasndaksndkajs a asd asd a as asd asd asd as a ad asdajdnadkandka a asd a
+     adkaknddja nkjandkjan jknasdjnopgkregepk3o,coqurgequtwz83ipwoktecg;e f
+      wklnf mlks;fk nsd ksdnfkms ;;fmkmflksmflks 
+    """
+    st.info(content)
