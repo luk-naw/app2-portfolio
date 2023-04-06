895fd55e5db79345891ec342dc72f47edf70a9db
lukasznawrocki <lukasz.nawrocki.pl@gmail.com>
2023-04-06 20:11:17 +0200

    Adding environment variable to hide sensitive information like password and user_name

--- send_email.py
+++ send_email.py
@@ -1,12 +1,13 @@
 import smtplib, ssl
+import os
 
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
-    password = "gjltthnpaetvigvk"
-    user_name = "lukasz.nawrocki.pl@gmail.com"
-    receiver = "lukasz.nawrocki.pl@gmail.com"
+    user_name = os.getenv("USER_NAME")
+    receiver = user_name
+    password = os.getenv("PASSWORD")
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
